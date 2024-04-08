64c4aa82a3d034a2755294bf331723e3f13e9d2d
diegwl <diegocastanlopes0210@outlook.com>
2024-04-08 10:52:21 -0300

    security and user entity

---
+++ remedios/src/main/java/com/diegwl/remedios/usuarios/Usuario.java
@@ -0,0 +1,22 @@
+package com.diegwl.remedios.usuarios;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Table(name = "Usuario")
+@Entity(name = "usuarios")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
---
+++ remedios/src/main/resources/db/migration/V3__create-table-usuario.sql
@@ -0,0 +1,7 @@
+create table usuario(
+id bigint not null auto_increment,
+login varchar(255) not null,
+senha varchar(255) not null,
+
+primary key (id)
+);
