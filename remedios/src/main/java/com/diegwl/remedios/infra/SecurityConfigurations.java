ac5560fdac374f3503c2616d468a794784341c64
diegwl <diegocastanlopes0210@outlook.com>
2024-04-08 11:16:29 -0300

    securit configuration

---
+++ remedios/src/main/java/com/diegwl/remedios/infra/SecurityConfigurations.java
@@ -0,0 +1,18 @@
+package com.diegwl.remedios.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable).sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+    }
+}
