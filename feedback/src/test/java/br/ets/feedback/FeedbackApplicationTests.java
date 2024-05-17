0bb9e80efd741f3f14e9c9e35513cd7a016914ce
diegwl <diegocastanlopes0210@outlook.com>
2024-05-17 10:45:34 -0300

    iniciando trilha spring ets

---
+++ feedback/src/test/java/br/ets/feedback/FeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ets.feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
